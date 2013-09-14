92d7d61c3372a76d5d372ff4e06c515c6b7ce4a7
Tommi Carleman <me@tommicarleman.net>
2013-09-14 23:10:20 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
                 },
                 files: [
                     // First copy everything from the images folder & then copy the crushed files over
-                    {expand: true, cwd: './less/', src: ['*', '!bootstrap-ie7.less', '!boxsizing.htc'], dest: './stylesheets', filter: 'isFile'},
+                    {expand: true, cwd: './less/', src: ['*', '!bootstrap-ie7.less', '!boxsizing.htc'], dest: './stylesheets', filter: 'isFile', rename: function(dest, src){ return dest + src.replace('.less', '.css'); }},
                 ]
             }
         },
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
             css: {
                 files: [
                     // Copy CSS
-                    {expand: true, cwd: './stylesheets/', src: ['**'], dest: 'project/stylesheets', filter: 'isFile', rename: function(dest, src){ return dest + src.replace('.less', '.css'); }},
+                    {expand: true, cwd: './stylesheets/', src: ['**'], dest: 'project/stylesheets', filter: 'isFile'},
                 ]
             },
 
